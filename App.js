2cc87bc2b011206ad4c4bf6fe1f69ab121991b8c
Dmitriy Shishkin <6ast3r@gmail.com>
2021-03-26 15:38:14 +0700

    Add react-native-gesture-handler

--- App.js
+++ App.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, {
+  useMemo,
+} from 'react';
+import {
+  PanGestureHandler,
+} from 'react-native-gesture-handler';
+import {
+  Animated,
+} from 'react-native';
 import { RNCamera } from 'react-native-camera';
 
-export default () => (
-  <RNCamera
-    captureAudio={false}
-    style={{ flex: 1 }}
-  />
-);
+export default () => {
+  const dragX = useMemo(() => new Animated.Value(0), []);
+
+  const onGestureEvent = useMemo(() => Animated.event(
+    [{ nativeEvent: { translationX: dragX } }],
+    { useNativeDriver: true },
+  ), [dragX]);
+
+  return (
+    <>
+      <PanGestureHandler
+        activeOffsetX={8}
+        onGestureEvent={onGestureEvent}
+        onHandlerStateChange={() => {}}
+      >
+        <Animated.View />
+      </PanGestureHandler>
+      <RNCamera
+        captureAudio={false}
+        style={{ flex: 1 }}
+      />
+    </>
+  );
+};
